94c15fc4aec59c82cbbc9a565bac43352f9196dd
Hakkeshawa <aleks.work@yahoo.com>
2024-05-15 16:25:25 +0800

    Initial commit

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -1,11 +1,12 @@
 <template>
-    <div class="character-card">
-        <img :src="character.image" :alt="character.name" />
-        <h2>{{ character.name }} </h2>
-        <p>Status: {{ character.status }}</p>
+  <div class="character-card">
+    <img :src="character.image" :alt="character.name" />
+    <h2>{{ character.name }}</h2>
+    <p>Status: {{ character.status }}</p>
     <p>Species: {{ character.species }}</p>
   </div>
 </template>
+
 <script>
 export default {
   props: {
